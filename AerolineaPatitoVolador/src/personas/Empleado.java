91fabaea2bf4daeae1d8f6e1240a4274154adc35
sefidoalker <2337469e@umich.mx>
2025-05-07 12:06:18 -0600

    PERSONAS COMPLETO

--- AerolineaPatitoVolador/src/personas/Empleado.java
+++ AerolineaPatitoVolador/src/personas/Empleado.java
@@ -1,5 +1,48 @@
 package personas;
 
-public class Empleado {
-    // TODO: Implementar atributos y métodos
+public class Empleado extends Usuario {
+
+    private String numeroEmpleado;
+    private String puesto;
+
+    public Empleado(String id, String nombre, String email, String contraseña,
+                    String numeroEmpleado, String puesto) {
+        super(id, nombre, email, contraseña);
+        this.numeroEmpleado = numeroEmpleado;
+        this.puesto = puesto;
+    }
+
+    public String getNumeroEmpleado() {
+        return numeroEmpleado;
+    }
+
+    public void setNumeroEmpleado(String numeroEmpleado) {
+        this.numeroEmpleado = numeroEmpleado;
+    }
+
+    public String getPuesto() {
+        return puesto;
+    }
+
+    public void setPuesto(String puesto) {
+        this.puesto = puesto;
+    }
+
+    // Método para gestionar un vuelo (puede actualizar estado, etc.)
+    public void gestionarVuelo(Vuelo vuelo) {
+        // Acción simulada
+        System.out.println("Empleado " + nombre + " gestionando el vuelo " + vuelo.getNumeroVuelo());
+        // Aquí podrías actualizar estado, horario, etc.
+    }
+
+    // Método para asignar un asiento a una reserva
+    public void asignarAsiento(Reservacion reservacion, Asiento asiento) {
+        if (asiento.estaDisponible()) {
+            reservacion.setAsiento(asiento);
+            asiento.reservar();
+            System.out.println("Asiento " + asiento.getNumero() + " asignado exitosamente.");
+        } else {
+            System.out.println("El asiento ya está ocupado.");
+        }
+    }
 }
